1bc46d95dfa8e4766cce3fc43ec37e2946d49506
Lorenzo Donini <lorenzo.donini@motius.de>
2019-06-11 23:00:39 +0200

    Add custom DateTime object, with support for custom format

--- ocpp/1.6/common.go
+++ ocpp/1.6/common.go
@@ -1,15 +1,52 @@
 package v16
 
-import "time"
+import (
+	"encoding/json"
+	"strings"
+	"time"
+)
 
 const (
 	ISO8601 = "2006-01-02T15:04:05Z"
 )
 
+type DateTime struct {
+	time.Time
+}
+
+var DateTimeFormat = ""
+
+func (dt *DateTime) UnmarshalJSON(input []byte) error {
+	strInput := string(input)
+	strInput = strings.Trim(strInput, `"`)
+	if DateTimeFormat == "" {
+		defaultTime := time.Time{}
+		err := json.Unmarshal(input, defaultTime)
+		if err != nil {
+			return err
+		}
+		dt.Time = defaultTime
+	} else {
+		newTime, err := time.Parse(DateTimeFormat, strInput)
+		if err != nil {
+			return err
+		}
+		dt.Time = newTime
+	}
+	return nil
+}
+
+func (dt *DateTime) MarshalJSON() ([]byte, error) {
+	if DateTimeFormat == "" {
+		return json.Marshal(dt.Time)
+	}
+	timeStr := dt.Time.Format(DateTimeFormat)
+	return json.Marshal(timeStr)
+}
+
 type PropertyViolation struct {
 	error
 	Property string
-
 }
 
 func (e* PropertyViolation) Error() string {
