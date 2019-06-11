3ef1f78a8593d797041df58f9a884655bcca7997
Lorenzo Donini <lorenzo.donini@motius.de>
2019-06-11 19:02:09 +0200

    Use suite mocks inside e2e boot notification test

--- test/boot_notification_test.go
+++ test/boot_notification_test.go
@@ -146,15 +146,13 @@ func (suite *OcppV16TestSuite) TestBootNotificationE2EMocked() {
 	responseJson := fmt.Sprintf(`[3,"%v",{"currentTime": "%v", "interval": 60, "status": "%v"}]`, messageId, time.Now().Format(v16.ISO8601), v16.RegistrationStatusAccepted)
 	requestRaw := []byte(requestJson)
 	responseRaw := []byte(responseJson)
-	server := MockWebsocketServer{}
-	client := MockWebsocketClient{}
 	channel := MockWebSocket{id: wsId}
 	// Setting server handlers
-	server.SetNewClientHandler(func(ws ws.Channel) {
+	suite.mockServer.SetNewClientHandler(func(ws ws.Channel) {
 		assert.NotNil(t, ws)
 		assert.Equal(t, wsId, ws.GetId())
 	})
-	server.SetMessageHandler(func(ws ws.Channel, data []byte) error {
+	suite.mockServer.SetMessageHandler(func(ws ws.Channel, data []byte) error {
 		assert.Equal(t, requestRaw, data)
 		jsonData := string(data)
 		assert.Equal(t, requestJson, jsonData)
@@ -162,17 +160,17 @@ func (suite *OcppV16TestSuite) TestBootNotificationE2EMocked() {
 		CheckCall(call, t, v16.BootNotificationFeatureName, messageId)
 		suite.chargePoint.AddPendingRequest(messageId, call.Payload)
 		// TODO: generate the response dynamically
-		err := client.messageHandler(responseRaw)
+		err := suite.mockClient.messageHandler(responseRaw)
 		assert.Nil(t, err)
 		return nil
 	})
 	// Setting client handlers
-	client.On("Start", mock.AnythingOfType("string")).Return().Run(func(args mock.Arguments) {
+	suite.mockClient.On("Start", mock.AnythingOfType("string")).Return().Run(func(args mock.Arguments) {
 		u := args.String(0)
 		assert.Equal(t, wsUrl, u)
-		server.newClientHandler(channel)
+		suite.mockServer.newClientHandler(channel)
 	})
-	client.SetMessageHandler(func(data []byte) error {
+	suite.mockClient.SetMessageHandler(func(data []byte) error {
 		assert.Equal(t, responseRaw, data)
 		jsonData := string(data)
 		assert.Equal(t, responseJson, jsonData)
@@ -180,15 +178,15 @@ func (suite *OcppV16TestSuite) TestBootNotificationE2EMocked() {
 		CheckCallResult(callResult, t, messageId)
 		return nil
 	})
-	client.On("Write", mock.Anything).Return().Run(func(args mock.Arguments) {
+	suite.mockClient.On("Write", mock.Anything).Return().Run(func(args mock.Arguments) {
 		data := args.Get(0)
 		bytes := data.([]byte)
 		assert.NotNil(t, bytes)
-		err := server.messageHandler(channel, bytes)
+		err := suite.mockServer.messageHandler(channel, bytes)
 		assert.Nil(t, err)
 	})
 	// Test Run
-	err := client.Start(wsUrl)
+	err := suite.mockClient.Start(wsUrl)
 	assert.Nil(t, err)
-	client.Write(requestRaw)
+	suite.mockClient.Write(requestRaw)
 }
