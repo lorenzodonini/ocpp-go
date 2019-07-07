39a00a1c52001b62adfd89f18a371633a8ce2052
Lorenzo Donini <lorenzo.donini@motius.de>
2019-07-07 19:01:43 +0200

    Move websocket test to ws package

--- ws/websocket_test.go
+++ ws/websocket_test.go
@@ -1,9 +1,8 @@
-package test
+package ws
 
 import (
 	"bytes"
 	"fmt"
-	"github.com/lorenzodonini/go-ocpp/ws"
 	"github.com/stretchr/testify/assert"
 	"net/url"
 	"testing"
@@ -16,9 +15,44 @@ const (
 	testPath   = "/ws/testws"
 )
 
+func NewWebsocketServer(t *testing.T, onMessage func(data []byte) ([]byte, error)) *Server {
+	wsServer := Server{}
+	wsServer.SetMessageHandler(func(ws Channel, data []byte) error {
+		assert.NotNil(t, ws)
+		assert.NotNil(t, data)
+		if onMessage != nil {
+			response, err := onMessage(data)
+			assert.Nil(t, err)
+			if response != nil {
+				err = wsServer.Write(ws.GetId(), data)
+				assert.Nil(t, err)
+			}
+		}
+		return nil
+	})
+	return &wsServer
+}
+
+func NewWebsocketClient(t *testing.T, onMessage func(data []byte) ([]byte, error)) *Client {
+	wsClient := Client{}
+	wsClient.SetMessageHandler(func(data []byte) error {
+		assert.NotNil(t, data)
+		if onMessage != nil {
+			response, err := onMessage(data)
+			assert.Nil(t, err)
+			if response != nil {
+				err = wsClient.Write(data)
+				assert.Nil(t, err)
+			}
+		}
+		return nil
+	})
+	return &wsClient
+}
+
 func TestWebsocketEcho(t *testing.T) {
 	message := []byte("Hello WebSocket!")
-	var wsServer *ws.Server
+	var wsServer *Server
 	wsServer = NewWebsocketServer(t, func(data []byte) ([]byte, error) {
 		assert.True(t, bytes.Equal(message, data))
 		return data, nil
@@ -36,7 +70,8 @@ func TestWebsocketEcho(t *testing.T) {
 	go func() {
 		timer := time.NewTimer(1 * time.Second)
 		<-timer.C
-		wsClient.Write(message)
+		err := wsClient.Write(message)
+		assert.Nil(t, err)
 	}()
 	done := make(chan bool)
 	// Wait for messages to be exchanged, then close connection
