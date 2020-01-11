0713b8dbef79a1755d3558457dbb95db9c2e4ded
Lorenzo Donini <lorenzo.donini@motius.de>
2020-01-11 07:22:05 +0100

    Implement test for wss

--- ws/websocket_test.go
+++ ws/websocket_test.go
@@ -2,9 +2,19 @@ package ws
 
 import (
 	"bytes"
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
+	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"math/big"
 	"net/url"
+	"os"
 	"testing"
 	"time"
 )
@@ -90,6 +100,66 @@ func TestWebsocketEcho(t *testing.T) {
 	wsServer.Stop()
 }
 
+func TestTLSWebsocketEcho(t *testing.T) {
+	message := []byte("Hello Secure WebSocket!")
+	var wsServer *Server
+	wsServer = NewWebsocketServer(t, func(data []byte) ([]byte, error) {
+		assert.True(t, bytes.Equal(message, data))
+		return data, nil
+	})
+	// Create self-signed TLS certificate
+	certFilename := "cert.pem"
+	keyFilename := "key.pem"
+	err := createTLSCertificate(certFilename, keyFilename)
+	assert.Nil(t, err)
+	defer os.Remove(certFilename)
+	defer os.Remove(keyFilename)
+
+	// Set self-signed TLS certificate
+	wsServer.tlsCertificatePath = certFilename
+	wsServer.tlsCertificateKey = keyFilename
+	go wsServer.Start(serverPort, serverPath)
+	time.Sleep(1 * time.Second)
+
+	// Test message
+	wsClient := NewWebsocketClient(t, func(data []byte) ([]byte, error) {
+		assert.True(t, bytes.Equal(message, data))
+		return nil, nil
+	})
+	host := fmt.Sprintf("localhost:%v", serverPort)
+	u := url.URL{Scheme: "wss", Host: host, Path: testPath}
+	// Wait for connection to be established, then send a message to server
+	go func() {
+		timer := time.NewTimer(1 * time.Second)
+		<-timer.C
+		err := wsClient.Write(message)
+		assert.Nil(t, err)
+	}()
+	done := make(chan bool)
+	// Wait for messages to be exchanged, then close connection
+	go func() {
+		timer := time.NewTimer(3 * time.Second)
+		<-timer.C
+		wsClient.Stop()
+		done <- true
+	}()
+	err = wsClient.Start(u.String(), func(dialer *websocket.Dialer) {
+		certPool := x509.NewCertPool()
+		data, err := ioutil.ReadFile(certFilename)
+		assert.Nil(t, err)
+		ok := certPool.AppendCertsFromPEM(data)
+		assert.True(t, ok)
+		dialer.TLSClientConfig = &tls.Config{
+			RootCAs: certPool,
+		}
+	})
+	assert.Nil(t, err)
+	result := <-done
+	assert.True(t, result)
+	// Cleanup
+	wsServer.Stop()
+}
+
 func TestWebsocketClientConnectionBreak(t *testing.T) {
 	newClient := make(chan bool)
 	disconnected := make(chan bool)
@@ -146,11 +216,70 @@ func TestWebsocketServerConnectionBreak(t *testing.T) {
 	// Test
 	wsClient := NewWebsocketClient(t, nil)
 	host := fmt.Sprintf("localhost:%v", serverPort)
-	u := url.URL{Scheme: "ws", Host: host, Path: testPath}
+	u := url.URL{Scheme: "wss", Host: host, Path: testPath}
 	err := wsClient.Start(u.String())
 	assert.Nil(t, err)
 	result := <-disconnected
 	assert.True(t, result)
 	// Cleanup
 	wsServer.Stop()
+}
+
+// Utility function
+func createTLSCertificate(certificateFilename string, keyFilename string) error {
+	// Generate ed25519 key-pair
+	publicKey, privateKey, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return err
+	}
+	// Create self-signed certificate
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return err
+	}
+	notBefore := time.Now()
+	notAfter := notBefore.Add(time.Hour * 24)
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"ocpp-go"},
+			CommonName: "localhost",
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, publicKey, privateKey)
+	if err != nil {
+		return err
+	}
+	// Save certificate to disk
+	certOut, err := os.Create(certificateFilename)
+	if err != nil {
+		return err
+	}
+	defer certOut.Close()
+	err = pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	if err != nil {
+		return err
+	}
+	// Save key to disk
+	keyOut, err := os.Create(keyFilename)
+	if err != nil {
+		return err
+	}
+	defer keyOut.Close()
+	privateBytes, err := x509.MarshalPKCS8PrivateKey(privateKey)
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(keyOut, &pem.Block{Type: "PRIVATE KEY", Bytes: privateBytes})
+	if err != nil {
+		return err
+	}
+	return nil
 }
