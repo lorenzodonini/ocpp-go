9ecb5e51786da10142aad089e72e373a7bf18ba1
Lorenzo Donini <lorenzo.donini@motius.de>
2020-09-18 15:18:22 +0200

    Add SANs to example certificate generation

--- example/1.6/create-test-certificates.sh
+++ example/1.6/create-test-certificates.sh
@@ -7,9 +7,9 @@ cd certs
 openssl req -new -x509 -nodes -days 120 -extensions v3_ca -keyout ca.key -out ca.crt -subj "/CN=ocpp-go-example"
 # Generate self-signed central-system certificate
 openssl genrsa -out cs/central-system.key 4096
-openssl req -out cs/central-system.csr -key cs/central-system.key -new -subj "/CN=central-system"
-openssl x509 -req -in cs/central-system.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out cs/central-system.crt -days 120
+openssl req -new -out cs/central-system.csr -key cs/central-system.key -config ../openssl-cs.conf
+openssl x509 -req -in cs/central-system.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out cs/central-system.crt -days 120 -extensions req_ext -extfile ../openssl-cs.conf
 # Generate self-signed charge-point certificate
 openssl genrsa -out cp/charge-point.key 4096
-openssl req -new -out cp/charge-point.csr -key cp/charge-point.key -subj "/CN=charge-point"
-openssl x509 -req -in cp/charge-point.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out cp/charge-point.crt -days 120
+openssl req -new -out cp/charge-point.csr -key cp/charge-point.key -config ../openssl-cp.conf
+openssl x509 -req -in cp/charge-point.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out cp/charge-point.crt -days 120 -extensions req_ext -extfile ../openssl-cp.conf
