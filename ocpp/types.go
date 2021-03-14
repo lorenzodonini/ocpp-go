31eea9832740bd76300e26b3bc6054d9d8bc1bc4
Lorenzo Donini <lorenzo.donini90@gmail.com>
2021-03-14 12:59:35 +0100

    Add custom logger interface

---
+++ ocpp/types.go
@@ -0,0 +1,24 @@
+package ocpp
+
+// Logger is the adapter interface that needs to be implemented, if the library should internally print logs.
+//
+// This allows to hook up your logger of choice.
+type Logger interface {
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+}
+
+// VoidLogger is an empty implementation of the Logger interface, which doesn't actually process any logs.
+// It may be used as a dummy implementation, if no logs should be visible.
+type VoidLogger struct{}
+
+func (l *VoidLogger) Debug(args ...interface{})                 {}
+func (l *VoidLogger) Debugf(format string, args ...interface{}) {}
+func (l *VoidLogger) Info(args ...interface{})                  {}
+func (l *VoidLogger) Infof(format string, args ...interface{})  {}
+func (l *VoidLogger) Error(args ...interface{})                 {}
+func (l *VoidLogger) Errorf(format string, args ...interface{}) {}
