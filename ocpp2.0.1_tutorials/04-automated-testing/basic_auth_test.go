efffbc767463b02e79221a20f2bdbe4902ceeb54
Fabio Miranda <fabio@miranti.net.br>
2022-02-28 21:19:25 -0300

    Tutorial: Testing CSMS and CS using a Goroutine

---
+++ ocpp2.0.1_tutorials/04-automated-testing/basic_auth_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/lorenzodonini/ocpp-go/ocpp2.0.1"
+	"github.com/lorenzodonini/ocpp-go/ws"
+	"github.com/stretchr/testify/require"
+)
+
+var server = ws.NewServer()
+var csms = ocpp2.NewCSMS(nil, server)
+
+var client = ws.NewClient()
+var cs = ocpp2.NewChargingStation("cs001", nil, client)
+
+func TestEnd2EndBasicAuth(t *testing.T) {
+	go csms.Start(7778, "/{id}")
+	defer server.Stop()
+
+	server.SetBasicAuthHandler(func(user string, pass string) bool {
+		return user == "cs001" && pass == "s3cr3t"
+	})
+	client.SetBasicAuth("cs001", "s3cr3t")
+
+	err := cs.Start("ws://localhost:7778")
+	require.Nil(t, err)
+}
